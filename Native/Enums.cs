d41df686be7215b8572cd708ab9d752da4b63d0c
Savas Celik <scelik.ch@gmail.com>
2022-11-29 00:23:06 +0100

    Separate native Structs/Enums/Functions

---
+++ Native/Enums.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CelSerEngine.Native
+{
+    internal static class Enums
+    {
+        /// <summary>
+        /// Full value list here:
+        /// https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-erref/596a1078-e883-4972-9bbc-49e60bebca55
+        /// https://www.pinvoke.net/default.aspx/Enums/NtStatus.html
+        /// </summary>
+        internal enum NTSTATUS : uint
+        {
+            Success = 0x00000000,
+            Error = 0xc0000000
+        }
+
+        /// <summary>
+        /// https://msdn.microsoft.com/en-us/library/windows/desktop/ms684880%28v=vs.85%29.aspx?f=255&MSPPError=-2147217396
+        /// </summary>
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            PROCESS_ALL_ACCESS = 0x001F0FFF,
+            PROCESS_CREATE_PROCESS = 0x0080,
+            PROCESS_CREATE_THREAD = 0x0002,
+            PROCESS_DUP_HANDLE = 0x0040,
+            PROCESS_QUERY_INFORMATION = 0x0400,
+            PROCESS_QUERY_LIMITED_INFORMATION = 0x1000,
+            PROCESS_SET_INFORMATION = 0x0200,
+            PROCESS_SET_QUOTA = 0x0100,
+            PROCESS_SUSPEND_RESUME = 0x0800,
+            PROCESS_TERMINATE = 0x0001,
+            PROCESS_VM_OPERATION = 0x0008,
+            PROCESS_VM_READ = 0x0010,
+            PROCESS_VM_WRITE = 0x0020,
+            SYNCHRONIZE = 0x00100000
+        }
+    }
+}
---
+++ Native/Structs.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace CelSerEngine.Native
+{
+    internal static class Structs
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct CLIENT_ID
+        {
+            internal IntPtr UniqueProcess;
+            internal IntPtr UniqueThread;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct SYSTEM_INFO
+        {
+            public ushort processorArchitecture;
+            ushort reserved;
+            public uint pageSize;
+            public IntPtr minimumApplicationAddress;
+            public IntPtr maximumApplicationAddress;
+            public IntPtr activeProcessorMask;
+            public uint numberOfProcessors;
+            public uint processorType;
+            public uint allocationGranularity;
+            public ushort processorLevel;
+            public ushort processorRevision;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct MEMORY_BASIC_INFORMATION64
+        {
+            public ulong BaseAddress;
+            public ulong AllocationBase;
+            public uint AllocationProtect;
+            public uint __alignment1;
+            public ulong RegionSize;
+            public uint State;
+            public uint Protect;
+            public uint Type;
+            public uint __alignment2;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct OBJECT_ATTRIBUTES
+        {
+            public int Length;
+            public int RootDirectory;
+            public IntPtr ObjectName;
+            public uint Attributes;
+            public int SecurityDescriptor;
+            public int SecurityQualityOfService;
+        }
+    }
+}
