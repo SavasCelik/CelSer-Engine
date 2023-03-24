fd70e4e4bf0859a2375af1feed32180f3416428b
Savas Celik <scelik.ch@gmail.com>
2023-03-24 22:59:44 +0100

    Remove not needed code

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -13,13 +13,5 @@ namespace CelSerEngine.Views
         {
             InitializeComponent();
         }
-
-        private void listView1_ColumnClick(object sender, MouseButtonEventArgs e)
-        {
-            var myListView = sender as ListView;
-            var te = myListView.InputHitTest(e.GetPosition(myListView)) as TextBlock;
-            var te2 = myListView.InputHitTest(e.GetPosition(myListView));
-            //MessageBox.Show("Column " + e.Column.ToString() + " Clicked");
-        }
     }
 }
