d23fd877b81b502503444feaee3adc0b934ddf6b
Savas Celik <scelik.ch@gmail.com>
2023-03-30 21:28:49 +0200

    Add new class ProcessMemory for representing a memory

---
+++ Models/ProcessMemory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CelSerEngine.Models;
+
+public class ProcessMemory
+{
+    public IntPtr BaseAddress { get; set; }
+    public int Offset { get; set; }
+    public IntPtr Address => BaseAddress + Offset;
+    public byte[] Memory { get; set; }
+}
