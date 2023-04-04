b46bee9e03ec65577aa6ad6f384c1eaee8dac0b1
Savas Celik <scelik.ch@gmail.com>
2023-04-05 00:34:27 +0200

    Move pointer scanning to PointerScanResultsViewModel

--- ViewModels/PointerScanResultsViewModel.cs
+++ ViewModels/PointerScanResultsViewModel.cs
@@ -1,7 +1,225 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CelSerEngine.Models;
+using CelSerEngine.Native;
+using CelSerEngine.Views;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
 
 namespace CelSerEngine.ViewModels;
 
 public partial class PointerScanResultsViewModel : ObservableRecipient
 {
+    private readonly SelectProcessViewModel _selectProcessViewModel;
+    private int _pointerSize;
+    public List<Pointer> FoundPointers { get; set; }
+
+    public PointerScanResultsViewModel(SelectProcessViewModel selectProcessViewModel)
+    {
+        _selectProcessViewModel = selectProcessViewModel;
+
+        _pointerSize = sizeof(long);
+    }
+
+    public void StartPointerScan(PointerScanOptionsViewModel pointerScanOptions)
+    {
+        var selectedProcess = _selectProcessViewModel.SelectedProcess!;
+        var maxSize = 4000;
+        var searchedAddress = long.Parse(pointerScanOptions.PointerScanAddress, NumberStyles.HexNumber);
+        var staticPointers = GetStaticPointers(selectedProcess);
+        var staticPointersByAddress = staticPointers.ToDictionary(x => x.Address);
+        var heapPointers = GetHeapPointers(selectedProcess);
+        var heapPointersByPointingTo = heapPointers
+            .OrderBy(x => x.PointingTo)
+            .GroupBy(x => x.PointingTo)
+            .ToDictionary(x => x.Key, x => x.ToArray());
+        var pointsWhereIWant = heapPointers.Where(x => x.Address.ToInt64() >= searchedAddress - maxSize && x.Address.ToInt64() <= searchedAddress).ToArray();
+        var pointerScan1 = new List<Pointer>();
+
+        foreach (var pointer in pointsWhereIWant)
+        {
+            var firstOffset = (IntPtr)(searchedAddress - pointer.Address.ToInt64());
+            pointer.Offsets.Add(firstOffset);
+            pointer.PointingTo = (IntPtr)searchedAddress;
+
+            if (heapPointersByPointingTo.TryGetValue(pointer.Address, out var pointers))
+            {
+                var clonedPointers = pointers.Select(x => x.Clone()).ToList();
+                clonedPointers.ForEach(p => p.Offsets.Add(firstOffset));
+                pointerScan1.AddRange(clonedPointers);
+            }
+        }
+
+        var pointingThere = new List<Pointer>();
+        var level = 4;
+        var counter = new Dictionary<IntPtr, int>();
+        var alreadyTracking = new HashSet<IntPtr>();
+
+        for (var currentLevel = 0; currentLevel < level; currentLevel++)
+        {
+            var pointerList = pointerScan1.OrderBy(x => x.Offsets.Last()).ToArray();
+            pointerScan1.Clear();
+            foreach (var pointer in pointerList)
+            {
+                if (staticPointersByAddress.TryGetValue(pointer.Address, out var startingPoint))
+                {
+                    var newPointingThere = new Pointer
+                    {
+                        ModuleName = startingPoint.ModuleName,
+                        BaseAddress = startingPoint.BaseAddress,
+                        BaseOffset = startingPoint.BaseOffset,
+                        Offsets = pointer.Offsets,
+                        PointingTo = (IntPtr)searchedAddress
+                    };
+                    pointingThere.Add(newPointingThere);
+                }
+
+                if (alreadyTracking.Contains(pointer.Address))
+                {
+                    continue;
+                }
+
+                if (currentLevel == level - 1)
+                {
+                    continue;
+                }
+
+                alreadyTracking.Add(pointer.Address);
+
+                var newAddy = IntPtr.Zero;
+                for (int i = 0; i < maxSize; i += 4)
+                {
+
+                    newAddy = pointer.Address - i;
+                    if (heapPointersByPointingTo.TryGetValue(newAddy, out var pointers))
+                    {
+                        var clonedPointers = pointers.Select(x => x.Clone()).ToList();
+                        var offsets = new List<IntPtr>(pointer.Offsets);
+                        offsets.Add((IntPtr)i);
+                        clonedPointers.ForEach(x => x.Offsets = new List<IntPtr>(offsets));
+                        pointerScan1.AddRange(clonedPointers);
+                        var countingFound = counter.TryGetValue(newAddy, out int count);
+                        if (!staticPointersByAddress.ContainsKey(clonedPointers.First().Address) && countingFound && count >= 3)
+                        {
+                            heapPointersByPointingTo.Remove(newAddy);
+                        }
+                        else
+                        {
+                            count++;
+                            if (!countingFound)
+                            {
+                                counter.Add(newAddy, count);
+                            }
+                            counter[newAddy] = count;
+                        }
+                    }
+                }
+            }
+        }
+
+        var resultsts = pointingThere.OrderBy(x => x.Offsets.Count).ToList();
+        FoundPointers = resultsts;
+        //-- rescan --//
+        var nextAddy = IntPtr.Zero;
+        staticPointers = GetStaticPointers(selectedProcess);
+        staticPointersByAddress = staticPointers.ToDictionary(x => x.Address);
+        heapPointers = GetHeapPointers(selectedProcess);
+        var heapPointerByAddress = heapPointers.GroupBy(x => x.Address).ToDictionary(x => x.Key, x => x.First());
+        var results0 = resultsts.Where(x => x.BaseOffset == 0x325b00 && x.Offsets.Count == 4).ToArray();
+
+        var foundPointer = new List<Pointer>();
+        foreach (var resultItem in resultsts)
+        {
+            if (staticPointersByAddress.TryGetValue(resultItem.Address, out var pointsTo))
+            {
+                for (int i = resultItem.Offsets.Count - 1; i >= 0; i--)
+                {
+                    var offset = resultItem.Offsets[i];
+                    var addressWithOffset = pointsTo.PointingTo + offset.ToInt32();
+                    if (addressWithOffset == nextAddy)
+                    {
+                        foundPointer.Add(resultItem);
+                    }
+                    else if (heapPointerByAddress.TryGetValue(addressWithOffset, out var pointsToWithOffset))
+                    {
+                        pointsTo = pointsToWithOffset;
+                    }
+                }
+            }
+        }
+
+        var resultsts2 = foundPointer.ToArray();
+    }
+
+
+
+    private IReadOnlyList<Pointer> GetStaticPointers(ProcessAdapter process)
+    {
+        var baseAddress = process.MainModule!.BaseAddress;
+        var regionSize = process.MainModule!.ModuleMemorySize;
+        var currentSize = 0;
+        var listOfBaseAddresses = new List<Pointer>();
+
+        while (currentSize < regionSize)
+        {
+            var buffer = NativeApi.ReadVirtualMemory(process.GetProcessHandle(), (IntPtr)baseAddress + currentSize, (uint)_pointerSize);
+            var foundAddress = BitConverter.ToInt64(buffer);
+            listOfBaseAddresses.Add(new Pointer
+            {
+                ModuleName = process.MainModule.ModuleName,
+                BaseAddress = baseAddress,
+                BaseOffset = currentSize,
+                Memory = buffer,
+                PointingTo = (IntPtr)foundAddress
+            });
+
+            currentSize += _pointerSize;
+        }
+
+        return listOfBaseAddresses;
+    }
+
+    private IReadOnlyList<Pointer> GetHeapPointers(ProcessAdapter process)
+    {
+        var pages = NativeApi.GatherVirtualPages(process.GetProcessHandle());
+        var allAddresses = new List<Pointer>();
+
+        foreach (var page in pages)
+        {
+            var pageSpan = page.Bytes.AsSpan();
+            for (int i = 0; i < (int)page.RegionSize; i += _pointerSize)
+            {
+                if (i + _pointerSize > (int)page.RegionSize)
+                {
+                    continue;
+                }
+                var bufferValue = BitConverter.ToInt64(page.Bytes, i);
+                var entry = new Pointer
+                {
+                    Memory = pageSpan.Slice(i, _pointerSize).ToArray(),
+                    BaseAddress = new IntPtr((long)page.BaseAddress),
+                    BaseOffset = i,
+                    PointingTo = (IntPtr)bufferValue
+                };
+                allAddresses.Add(entry);
+            }
+        }
+
+        return allAddresses
+            .Where(x => x.PointingTo != IntPtr.Zero && x.Address.ToInt64() % 4 == 0)
+            //.Where(x => x.PointingTo.ToInt64() >= 0x10000 && x.PointingTo.ToInt64() <= 0x7ffffffeffff)
+            .ToArray();
+    }
+
+    public bool ShowPointerScanResultsDialog()
+    {
+        var selectProcessWidnwow = new PointerScanResults
+        {
+            Owner = App.Current.MainWindow
+        };
+
+        return selectProcessWidnwow.ShowDialog() ?? false;
+    }
 }
