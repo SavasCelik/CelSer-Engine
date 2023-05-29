4d8d8983c36a2268f02236ba4647aee1ec4bf407
Savas Celik <scelik.ch@gmail.com>
2023-05-29 19:28:15 +0200

    Add MemoryScanService

---
+++ src/CelSerEngine.Wpf/Services/IMemoryScanService.cs
@@ -0,0 +1,10 @@
+﻿using CelSerEngine.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+
+namespace CelSerEngine.Wpf.Services;
+public interface IMemoryScanService
+{
+    public Task<IReadOnlyCollection<IProcessMemory>> ScanProcessMemory(ScanConstraint scanConstraint, IntPtr processHandle, IProgress<float> progressUpdater);
+}
---
+++ src/CelSerEngine.Wpf/Services/MemoryScanService.cs
@@ -0,0 +1,23 @@
+﻿using CelSerEngine.Core.Comparators;
+using CelSerEngine.Core.Models;
+using CelSerEngine.Core.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CelSerEngine.Wpf.Services;
+public class MemoryScanService : IMemoryScanService
+{
+    public async Task<IReadOnlyCollection<IProcessMemory>> ScanProcessMemory(ScanConstraint scanConstraint, IntPtr processHandle, IProgress<float> progressUpdater)
+    {
+        var matchingMemories = await Task.Run(() =>
+        {
+            var pages = NativeApi.GatherVirtualPages(processHandle).ToArray();
+            var comparer = ComparerFactory.CreateVectorComparer(scanConstraint);
+            return comparer.GetMatchingValueAddresses(pages, progressUpdater);
+        }).ConfigureAwait(false);
+
+        return matchingMemories;
+    }
+}
