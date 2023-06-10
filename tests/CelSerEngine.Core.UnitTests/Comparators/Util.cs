97686e341c05581e434e7bca1bf1b4a95b3f9591
Savas Celik <scelik.ch@gmail.com>
2023-06-10 16:34:10 +0200

    Add vector camparer tests

---
+++ tests/CelSerEngine.Core.UnitTests/Comparators/Util.cs
@@ -0,0 +1,83 @@
+﻿using System.Numerics;
+
+namespace CelSerEngine.Core.UnitTests.Comparators;
+
+public static class Util
+{
+    private static Random s_random = Random.Shared;
+
+    public static T[] GenerateRandomValues<T>(int numValues, int min = 1, int max = 100) where T : struct
+    {
+        T[] values = new T[numValues];
+        for (int g = 0; g < numValues; g++)
+        {
+            values[g] = GenerateSingleValue<T>(min, max);
+        }
+
+        return values;
+    }
+
+    public static T GenerateSingleValue<T>(int min = 1, int max = 100) where T : struct
+    {
+        var randomRange = s_random.Next(min, max);
+        T value = unchecked((T)(dynamic)randomRange);
+        return value;
+    }
+
+    public static T[] GenerateRandomValuesForVector<T>(int? numValues = null) where T : struct
+    {
+        int minValue = GetMinValue<T>();
+        int maxValue = GetMaxValue<T>();
+        return GenerateRandomValues<T>(numValues ?? Vector<T>.Count, minValue, maxValue);
+    }
+
+    internal static int GetMinValue<T>() where T : struct
+    {
+        if (typeof(T) == typeof(int) || typeof(T) == typeof(long) || typeof(T) == typeof(float) || typeof(T) == typeof(double) || typeof(T) == typeof(uint) || typeof(T) == typeof(ulong))
+        {
+            return int.MinValue;
+        }
+        else if (typeof(T) == typeof(byte))
+        {
+            return byte.MinValue;
+        }
+        else if (typeof(T) == typeof(sbyte))
+        {
+            return sbyte.MinValue;
+        }
+        else if (typeof(T) == typeof(short))
+        {
+            return short.MinValue;
+        }
+        else if (typeof(T) == typeof(ushort))
+        {
+            return ushort.MinValue;
+        }
+        throw new NotSupportedException();
+    }
+
+    internal static int GetMaxValue<T>() where T : struct
+    {
+        if (typeof(T) == typeof(int) || typeof(T) == typeof(long) || typeof(T) == typeof(float) || typeof(T) == typeof(double) || typeof(T) == typeof(uint) || typeof(T) == typeof(ulong))
+        {
+            return int.MaxValue;
+        }
+        else if (typeof(T) == typeof(byte))
+        {
+            return byte.MaxValue;
+        }
+        else if (typeof(T) == typeof(sbyte))
+        {
+            return sbyte.MaxValue;
+        }
+        else if (typeof(T) == typeof(short))
+        {
+            return short.MaxValue;
+        }
+        else if (typeof(T) == typeof(ushort))
+        {
+            return ushort.MaxValue;
+        }
+        throw new NotSupportedException();
+    }
+}
--- tests/CelSerEngine.Core.UnitTests/Comparators/VectorComparerTests.cs
+++ tests/CelSerEngine.Core.UnitTests/Comparators/VectorComparerTests.cs
@@ -1,5 +1,6 @@
 ﻿using CelSerEngine.Core.Comparators;
 using CelSerEngine.Core.Models;
+using System.Numerics;
 using System.Runtime.InteropServices;
 using Xunit;
 
@@ -7,31 +8,135 @@ namespace CelSerEngine.Core.UnitTests.Comparators;
 
 public class VectorComparerTests
 {
-    [Fact]
-    public void GetMatchingMemorySegments_Integer_ExactValue_ReturnsCorrectCount()
+    [Theory]
+    [MemberData(nameof(ScanCompareTypes))]
+    public void CompareTo_Short_ReturnsCorrectCount(ScanCompareType scanCompareType)
     {
-        // Arrange
-        var searchedValueInt = 452;
-        var searchedValueString = "452";
-        var scanConstraint = new ScanConstraint(ScanCompareType.ExactValue, ScanDataType.Integer, searchedValueString);
-        var intArray = new int[32]
-        { 
-            2435745, 345834, searchedValueInt, 123, 54654, searchedValueInt, 2394234, 32492395, 786, 718345,
-            9609456, 349539, 3405345, searchedValueInt, 9353495, 35687, 48, 2132, 12365, 4546,
-            3634, 567567, 7844, 2134111, 357305, 34590341, searchedValueInt, 873544, 345345, 12356,
-            7007, 77784,
-        };
-        var byteArray = MemoryMarshal.AsBytes(intArray.AsSpan()).ToArray(); // Convert int array to byte array
+        RunRandomizedComparToTest<short>(scanCompareType, ScanDataType.Short);
+    }
+
+    [Theory]
+    [MemberData(nameof(ScanCompareTypes))]
+    public void CompareTo_Integer_ReturnsCorrectCount(ScanCompareType scanCompareType)
+    {
+        RunRandomizedComparToTest<int>(scanCompareType, ScanDataType.Integer);
+    }
+
+    [Theory]
+    [MemberData(nameof(ScanCompareTypes))]
+    public void CompareTo_Long_ReturnsCorrectCount(ScanCompareType scanCompareType)
+    {
+        RunRandomizedComparToTest<long>(scanCompareType, ScanDataType.Long);
+    }
+
+    [Theory]
+    [MemberData(nameof(ScanCompareTypes))]
+    public void CompareTo_Float_ReturnsCorrectCount(ScanCompareType scanCompareType)
+    {
+        RunRandomizedComparToTest<float>(scanCompareType, ScanDataType.Float);
+    }
+
+    [Theory]
+    [MemberData(nameof(ScanCompareTypes))]
+    public void CompareTo_Double_ReturnsCorrectCount(ScanCompareType scanCompareType)
+    {
+        RunRandomizedComparToTest<double>(scanCompareType, ScanDataType.Double);
+    }
+
+    private void RunRandomizedComparToTest<T>(ScanCompareType compareType, ScanDataType scanDataType)
+        where T : struct, INumber<T>
+    {
+        T[] values = Util.GenerateRandomValuesForVector<T>();
+        int searchedValueIndex = Util.GenerateSingleValue<int>(0, values.Length - 1);
+        var searchedValue = values[searchedValueIndex];
+        var bytes = MemoryMarshal.AsBytes(values.AsSpan());
+        var resultArray = new T[values.Length];
+        var scanConstraint = new ScanConstraint(compareType, scanDataType, searchedValue.ToString() ?? "");
+        var vectorComparer = new VectorComparer<T>(scanConstraint);
+
+        var result = vectorComparer.CompareTo(bytes).As<byte, T>();
+        result.CopyTo(resultArray);
+
+        var expectedCount = GetExpectedCount(compareType, values, searchedValue);
+        var actualCount = resultArray.Count(x => x != T.Zero);
+
+        Assert.Equal(expectedCount, actualCount);
+    }
+
+    [Theory]
+    [MemberData(nameof(ScanCompareTypes))]
+    public void GetMatchingMemorySegments_Short_ReturnsCorrectCount(ScanCompareType scanCompareType)
+    {
+        RunRandomizedGetMatchingMemorySegmentsTest<short>(scanCompareType, ScanDataType.Short);
+    }
+
+    [Theory]
+    [MemberData(nameof(ScanCompareTypes))]
+    public void GetMatchingMemorySegments_Integer_ReturnsCorrectCount(ScanCompareType scanCompareType)
+    {
+        RunRandomizedGetMatchingMemorySegmentsTest<int>(scanCompareType, ScanDataType.Integer);
+    }
+
+    [Theory]
+    [MemberData(nameof(ScanCompareTypes))]
+    public void GetMatchingMemorySegments_Long_ReturnsCorrectCount(ScanCompareType scanCompareType)
+    {
+        RunRandomizedGetMatchingMemorySegmentsTest<long>(scanCompareType, ScanDataType.Long);
+    }
+
+    [Theory]
+    [MemberData(nameof(ScanCompareTypes))]
+    public void GetMatchingMemorySegments_Float_ReturnsCorrectCount(ScanCompareType scanCompareType)
+    {
+        RunRandomizedGetMatchingMemorySegmentsTest<float>(scanCompareType, ScanDataType.Float);
+    }
+
+    [Theory]
+    [MemberData(nameof(ScanCompareTypes))]
+    public void GetMatchingMemorySegments_Double_ReturnsCorrectCount(ScanCompareType scanCompareType)
+    {
+        RunRandomizedGetMatchingMemorySegmentsTest<double>(scanCompareType, ScanDataType.Double);
+    }
+
+    private void RunRandomizedGetMatchingMemorySegmentsTest<T>(ScanCompareType scanCompareType, ScanDataType scanDataType)
+        where T : struct, INumber<T>
+    {
+        T[] values = Util.GenerateRandomValuesForVector<T>(Vector<T>.Count * 50);
+        var searchedValueIndex = Util.GenerateSingleValue<int>(0, values.Length);
+        var searchedValue = values[searchedValueIndex];
+        var scanConstraint = new ScanConstraint(scanCompareType, scanDataType, searchedValue.ToString() ?? "");
+        var byteArray = MemoryMarshal.AsBytes(values.AsSpan()).ToArray();
         var virtualMemoryRegions = new List<VirtualMemoryRegion>()
         {
             new VirtualMemoryRegion(new IntPtr(0x1337), (ulong)byteArray.Length, byteArray)
         };
-        var comparer = new VectorComparer<int>(scanConstraint);
+        var comparer = new VectorComparer<T>(scanConstraint);
 
         // Act
         IList<IMemorySegment> foundSegments = comparer.GetMatchingMemorySegments(virtualMemoryRegions, null);
 
         // Assert
-        Assert.Equal(4, foundSegments.Count);
+        var expectedCount = GetExpectedCount(scanCompareType, values, searchedValue);
+        Assert.Equal(expectedCount, foundSegments.Count);
+    }
+
+    public static IEnumerable<object[]> ScanCompareTypes()
+    {
+        yield return new object[] { ScanCompareType.ExactValue };
+        yield return new object[] { ScanCompareType.SmallerThan };
+        yield return new object[] { ScanCompareType.BiggerThan };
+    }
+
+    private int GetExpectedCount<T>(ScanCompareType compareType, T[] values, T searchedValue)
+        where T : struct, INumber<T>
+    {
+        if (compareType == ScanCompareType.ExactValue)
+            return values.Count(x => x == searchedValue);
+        else if (compareType == ScanCompareType.SmallerThan)
+            return values.Count(x => x < searchedValue);
+        else if (compareType == ScanCompareType.BiggerThan)
+            return values.Count(x => x > searchedValue);
+
+        return 0;
     }
 }
