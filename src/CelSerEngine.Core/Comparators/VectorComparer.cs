80b5a51539f1ab3442b11fffda62b26b63e72775
Savas Celik <scelik.ch@gmail.com>
2023-06-11 12:26:20 +0200

    Refactor VectorComparer

--- src/CelSerEngine.Core/Comparators/VectorComparer.cs
+++ src/CelSerEngine.Core/Comparators/VectorComparer.cs
@@ -35,33 +35,8 @@ public class VectorComparer<T> : IScanComparer where T : struct, INumber<T>
         for (var regionIndex = 0; regionIndex < virtualMemoryRegions.Count; regionIndex++)
         {
             var virtualMemoryRegion = virtualMemoryRegions[regionIndex];
-            // TODO: search the remaining section with a normal for loop
-            var remaining = (int)virtualMemoryRegion.RegionSize % Vector<byte>.Count;
-            var regionBytesAsSpan = virtualMemoryRegion.Bytes.AsSpan();
-
-            for (var i = 0; i < (int)virtualMemoryRegion.RegionSize - remaining; i += Vector<byte>.Count)
-            {
-                var splitBuffer = regionBytesAsSpan.Slice(i, Vector<byte>.Count);
-                var compareResult = CompareTo(splitBuffer);
-
-                if (!compareResult.Equals(Vector<byte>.Zero))
-                {
-                    for (var j = 0; j < Vector<byte>.Count; j += _sizeOfT)
-                    {
-                        if (compareResult[j] != 0)
-                        {
-                            var offset = i + j;
-                            var memoryValue = regionBytesAsSpan.Slice(offset, _sizeOfT).ConvertToString(_scanConstraint.ScanDataType);
-
-                            matchingProcessMemories.Add(
-                                new MemorySegment(
-                                    virtualMemoryRegion.BaseAddress, offset,
-                                    memoryValue,
-                                    _scanConstraint.ScanDataType));
-                        }
-                    }
-                }
-            }
+            var foundMatchingSegments = FindMatchingMemorySegmentsInRegion(virtualMemoryRegion);
+            matchingProcessMemories.AddRange(foundMatchingSegments);
 
             if (progressBarUpdater != null)
             {
@@ -71,5 +46,36 @@ public class VectorComparer<T> : IScanComparer where T : struct, INumber<T>
         }
 
         return matchingProcessMemories;
+    }
+
+    private IEnumerable<IMemorySegment> FindMatchingMemorySegmentsInRegion(VirtualMemoryRegion virtualMemoryRegion)
+    {
+        var remaining = (int)virtualMemoryRegion.RegionSize % Vector<byte>.Count;
+        var regionBytes = virtualMemoryRegion.Bytes;
+
+        for (var i = 0; i < (int)virtualMemoryRegion.RegionSize - remaining; i += Vector<byte>.Count)
+        {
+            var splitBuffer = regionBytes.AsSpan().Slice(i, Vector<byte>.Count);
+            var compareResult = CompareTo(splitBuffer);
+
+            foreach (var vectorIndex in FindMatchingVectorIndexes(compareResult))
+            {
+                var offset = i + vectorIndex;
+                var memoryValue = regionBytes.AsSpan().Slice(offset, _sizeOfT).ConvertToString(_scanConstraint.ScanDataType);
+                yield return new MemorySegment(virtualMemoryRegion.BaseAddress, offset, memoryValue, _scanConstraint.ScanDataType);
+            }
+        }
+    }
+
+    private IEnumerable<int> FindMatchingVectorIndexes(Vector<byte> vectorResult)
+    {
+        if (vectorResult.Equals(Vector<byte>.Zero))
+            yield break;
+
+        for (var j = 0; j < Vector<byte>.Count; j += _sizeOfT)
+        {
+            if (vectorResult[j] != 0)
+                yield return j;
+        }
     }
 }
