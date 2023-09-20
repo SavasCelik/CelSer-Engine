88a7454e9c95169672ad5cc43afe9d6fd077a58a
SavasCelik <scelik.ch@gmail.com>
2023-09-21 00:00:21 +0200

    Add ObservableScript

---
+++ src/CelSerEngine.Wpf/Models/ObservableScript.cs
@@ -0,0 +1,17 @@
+﻿using CelSerEngine.Core.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CelSerEngine.Wpf.Models;
+
+[ObservableObject]
+public partial class ObservableScript : BaseScript
+{
+    [ObservableProperty]
+    private bool _isActivated;
+
+}
