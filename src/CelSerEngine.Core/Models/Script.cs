7f2bb1c50945cdcb359e7c11efeea2c8f4ba1f87
SavasCelik <scelik.ch@gmail.com>
2023-09-24 16:30:58 +0200

    Make TargetProcess model nullable

--- src/CelSerEngine.Core/Models/Script.cs
+++ src/CelSerEngine.Core/Models/Script.cs
@@ -2,5 +2,5 @@
 public class Script : BaseScript
 {
     public int TargetProcessId { get; set; }
-    public required TargetProcess TargetProcess { get; set; }
+    public TargetProcess? TargetProcess { get; set; }
 }
