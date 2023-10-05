31812388f2ed07a0c68a968d723837f2a0ea8982
SavasCelik <scelik.ch@gmail.com>
2023-10-06 01:02:32 +0200

    Add Code-Completion

---
+++ src/CelSerEngine.Wpf/Models/EditorCompletionData.cs
@@ -0,0 +1,35 @@
+﻿using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Editing;
+using System;
+using System.Windows.Media;
+
+namespace CelSerEngine.Wpf.Models;
+
+/// Implements AvalonEdit ICompletionData interface to provide the entries in the
+/// completion drop down.
+public class EditorCompletionData : ICompletionData
+{
+    public ImageSource? Image => null;
+
+    public string Text { get; }
+
+    // Use this property if you want to show a fancy UIElement in the list.
+    public object Content => Text;
+
+    public object Description { get; }
+
+    public double Priority => 1;
+
+    public EditorCompletionData(string text, string description = "")
+    {
+        Text = text;
+        Description = description;
+    }
+
+
+    public void Complete(TextArea textArea, ISegment completionSegment, EventArgs insertionRequestEventArgs)
+    {
+        textArea.Document.Replace(completionSegment, Text);
+    }
+}
--- src/CelSerEngine.Wpf/Views/ScriptEditorWindow.xaml.cs
+++ src/CelSerEngine.Wpf/Views/ScriptEditorWindow.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using CelSerEngine.Core.Scripting;
+using CelSerEngine.Wpf.Models;
+using ICSharpCode.AvalonEdit.CodeCompletion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,9 +23,18 @@ namespace CelSerEngine.Wpf.Views;
 /// </summary>
 public partial class ScriptEditorWindow : Window
 {
+    [GeneratedRegex("\\bvar\\s+(\\w+)\\s*=")]
+    private static partial Regex VariableNameRegex();
+    [GeneratedRegex("\\b\\w+\\s+(\\w+)\\s*\\(")]
+    private static partial Regex MethodNameRegex();
+    private CompletionWindow? _completionWindow;
+    private readonly char[] _allowedCharsBeforeCompletion = { ' ', '\n', '\t', '(' };
+
     public ScriptEditorWindow()
     {
         InitializeComponent();
+        textEditor.TextArea.TextEntering += textEditor_TextArea_TextEntering;
+        textEditor.TextArea.TextEntered += textEditor_TextArea_TextEntered;
     }
 
     public string GetText()
@@ -32,5 +46,106 @@ public partial class ScriptEditorWindow : Window
     {
         textEditor.Text = text;
     }
+
+    private void textEditor_TextArea_TextEntered(object sender, TextCompositionEventArgs e)
+    {
+        IEnumerable<string> definedVariables = GetInTextDefinedVariables();
+        IEnumerable<string> definedMethods = GetInTextDefinedMethods();
+        IEnumerable<string> preDefinedVariables = GetPreDefinedVariables();
+        var foundDefinitions = preDefinedVariables
+            .Concat(definedVariables)
+            .Concat(definedMethods)
+            .Where(x => x.Contains(e.Text, StringComparison.InvariantCultureIgnoreCase))
+            .ToArray();
+
+        if (_completionWindow != null && _completionWindow.CompletionList.ListBox.Items.Count <= 0)
+            _completionWindow.Close();
+
+        if (_completionWindow != null)
+            return;
+
+        var lastWordIndex = Math.Max(textEditor.CaretOffset - 2, 0);
+
+        if (_allowedCharsBeforeCompletion.Contains(textEditor.Text[lastWordIndex]) && foundDefinitions.Any())
+        {
+            CreateCompletionWindow(foundDefinitions);
+            _completionWindow!.StartOffset -= 1;
+        }
+        else if (e.Text == ".")
+        {
+            var preDefinedMethods = GetPreDefinedMethods().ToArray();
+            CreateCompletionWindow(preDefinedMethods);
+        }
+        
+    }
+
+    private void CreateCompletionWindow(ICollection<string> completionStrings)
+    {
+        _completionWindow = new CompletionWindow(textEditor.TextArea);
+        IList<ICompletionData> data = _completionWindow.CompletionList.CompletionData;
+
+        foreach (var definition in completionStrings)
+        {
+            data.Add(new EditorCompletionData(definition));
+        }
+
+        _completionWindow.Show();
+        _completionWindow.Closed += delegate
+        {
+            _completionWindow = null;
+        };
+    }
+
+    private IEnumerable<string> GetInTextDefinedVariables()
+    {
+        // Extract variables from textEditor's text:
+        var regex = VariableNameRegex();
+
+        foreach (Match match in regex.Matches(textEditor.Text))
+        {
+            yield return match.Groups[1].Value;
+        }
+    }
+
+    private IEnumerable<string> GetInTextDefinedMethods()
+    {
+        // Extract methods from textEditor's text:
+        var regex = MethodNameRegex();
+
+        foreach (Match match in regex.Matches(textEditor.Text))
+        {
+            yield return match.Groups[1].Value;
+        }
+    }
+
+    private static IEnumerable<string> GetPreDefinedVariables()
+    {
+        const string memoryManagerName = nameof(MemoryManager);
+        yield return char.ToLowerInvariant(memoryManagerName[0]) + memoryManagerName[1..];
+    }
+
+    private static IEnumerable<string> GetPreDefinedMethods()
+    {
+        var memoryManagerMethods = typeof(MemoryManager).GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+
+        foreach (MethodInfo methodInfo in memoryManagerMethods)
+        {
+            yield return methodInfo.Name;
+        }
+    }
+
+    private void textEditor_TextArea_TextEntering(object sender, TextCompositionEventArgs e)
+    {
+        if (e.Text.Length > 0 && _completionWindow != null)
+        {
+            if (!char.IsLetterOrDigit(e.Text[0]))
+            {
+                // Whenever a non-letter is typed while the completion window is open,
+                // insert the currently selected element.
+                _completionWindow.CompletionList.RequestInsertion(e);
+            }
+        }
+        // Do not set e.Handled=true.
+        // We still want to insert the character that was typed.
     }
 }
