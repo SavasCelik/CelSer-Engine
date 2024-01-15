876ccb0eaec15dc7172d01b98f96b7886d01f201
Savas Celik <scelik.ch@gmail.com>
2024-01-15 21:12:55 +0100

    Add parallel pointer scan

--- src/CelSerEngine.Core/Scanners/PointerScanner.cs
+++ src/CelSerEngine.Core/Scanners/PointerScanner.cs
@@ -1,6 +1,7 @@
 ﻿using CelSerEngine.Core.Models;
 using CelSerEngine.Core.Native;
 using System.Buffers;
+using System.Collections.Concurrent;
 
 namespace CelSerEngine.Core.Scanners;
 
@@ -9,114 +10,170 @@ public class PointerScanner
     private readonly INativeApi _nativeApi;
     private readonly int _pointerSize;
     private readonly ArrayPool<byte> _byteArrayPool;
+    private List<Pointer> _pointerList;
+    private IDictionary<IntPtr, Pointer> _staticPointersByAddress;
+    private IDictionary<IntPtr, Pointer[]> _heapPointersByPointingTo;
+    private IDictionary<IntPtr, int> _pathCounter;
 
     public PointerScanner(INativeApi nativeApi)
     {
         _byteArrayPool = ArrayPool<byte>.Shared;
         _nativeApi = nativeApi;
         _pointerSize = sizeof(long);
+        _pointerList = new List<Pointer>();
+        _staticPointersByAddress = new Dictionary<IntPtr, Pointer>();
+        _heapPointersByPointingTo = new Dictionary<IntPtr, Pointer[]>();
+        _pathCounter = new Dictionary<IntPtr, int>();
     }
 
     public async Task<IList<Pointer>> ScanForPointersAsync(PointerScanOptions pointerScanOptions)
     {
-        var result = await Task.Run(() =>
+        var result = await Task.Run(() => ScanPointers(pointerScanOptions, useParallel: false)).ConfigureAwait(false);
+        return result;
+    }
+
+    public async Task<IList<Pointer>> ScanForPointersParallelAsync(PointerScanOptions pointerScanOptions)
+    {
+        var result = await Task.Run(() => ScanPointers(pointerScanOptions, useParallel: true)).ConfigureAwait(false);
+        return result;
+    }
+
+    private void SetupPointerScan(PointerScanOptions pointerScanOptions)
+    {
+        var staticPointers = GetStaticPointers(pointerScanOptions.ProcessId, pointerScanOptions.ProcessHandle);
+        _staticPointersByAddress = staticPointers.ToDictionary(x => x.Address);
+        var heapPointers = GetHeapPointers(pointerScanOptions.ProcessHandle);
+        _heapPointersByPointingTo = heapPointers
+            .OrderBy(x => x.PointingTo)
+            .GroupBy(x => x.PointingTo)
+            .ToDictionary(x => x.Key, x => x.ToArray());
+        var pointersInRelevantRange = heapPointers
+            .Where(x => x.Address.ToInt64() >= pointerScanOptions.SearchedAddress - pointerScanOptions.MaxOffset && x.Address.ToInt64() <= pointerScanOptions.SearchedAddress)
+            .ToArray();
+        _pointerList = new List<Pointer>();
+
+        foreach (var pointer in pointersInRelevantRange)
         {
-            var staticPointers = GetStaticPointers(pointerScanOptions.ProcessId, pointerScanOptions.ProcessHandle);
-            var staticPointersByAddress = staticPointers.ToDictionary(x => x.Address);
-            var heapPointers = GetHeapPointers(pointerScanOptions.ProcessHandle);
-            var heapPointersByPointingTo = heapPointers
-                .OrderBy(x => x.PointingTo)
-                .GroupBy(x => x.PointingTo)
-                .ToDictionary(x => x.Key, x => x.ToArray());
-            var pointsWhereIWant = heapPointers
-                .Where(x => x.Address.ToInt64() >= pointerScanOptions.SearchedAddress - pointerScanOptions.MaxOffset && x.Address.ToInt64() <= pointerScanOptions.SearchedAddress)
-                .ToArray();
-            var pointerScan1 = new List<Pointer>();
+            var firstOffset = pointerScanOptions.SearchedAddress - pointer.Address;
+            pointer.Offsets.Add(firstOffset);
+            pointer.PointingTo = pointerScanOptions.SearchedAddress;
 
-            foreach (var pointer in pointsWhereIWant)
+            if (_heapPointersByPointingTo.TryGetValue(pointer.Address, out var pointers))
             {
-                var firstOffset = pointerScanOptions.SearchedAddress - pointer.Address;
-                pointer.Offsets.Add(firstOffset);
-                pointer.PointingTo = pointerScanOptions.SearchedAddress;
-
-                if (heapPointersByPointingTo.TryGetValue(pointer.Address, out var pointers))
+                foreach (var p in pointers)
                 {
-                    var clonedPointers = pointers.Select(x => x.Clone()).ToList();
-                    clonedPointers.ForEach(p => p.Offsets.Add(firstOffset));
-                    pointerScan1.AddRange(clonedPointers);
+                    var clonedPointer = p.Clone();
+                    clonedPointer.Offsets.Add(firstOffset);
+                    _pointerList.Add(clonedPointer);
                 }
             }
+        }
 
-            var pointingThere = new List<Pointer>();
-            var counter = new Dictionary<IntPtr, int>();
-            var alreadyTracking = new HashSet<IntPtr>();
+        _pointerList = _pointerList.OrderBy(x => x.Offsets.Last()).ToList();
+        _pathCounter = new ConcurrentDictionary<IntPtr, int>();
+    }
 
-            for (var currentLevel = 0; currentLevel < pointerScanOptions.MaxLevel; currentLevel++)
+    private IList<Pointer> ScanPointers(PointerScanOptions pointerScanOptions, bool useParallel)
+    {
+        SetupPointerScan(pointerScanOptions);
+        var pointerWithStaticPointerPaths = new List<Pointer>();
+
+        for (var currentLevel = 0; currentLevel < pointerScanOptions.MaxLevel; currentLevel++)
+        {
+            var pointerList = _pointerList.OrderBy(x => x.Offsets.Last()).ToArray();
+            _pointerList.Clear();
+
+            if (useParallel)
             {
-                var pointerList = pointerScan1.OrderBy(x => x.Offsets.Last()).ToArray();
-                pointerScan1.Clear();
+                Parallel.ForEach(pointerList, (pointer) => ProcessPointer(pointer, pointerWithStaticPointerPaths, pointerScanOptions, currentLevel));
+            }
+            else
+            {
                 foreach (var pointer in pointerList)
                 {
-                    if (staticPointersByAddress.TryGetValue(pointer.Address, out var startingPoint))
+                    ProcessPointer(pointer, pointerWithStaticPointerPaths, pointerScanOptions, currentLevel);
+                }
+            }
+        }
+
+        return pointerWithStaticPointerPaths.OrderBy(x => x.Offsets.Count).ToList();
+    }
+
+    private void ProcessPointer(Pointer pointer, List<Pointer> pointerWithStaticPointerPaths, PointerScanOptions pointerScanOptions, int currentLevel)
+    {
+        if (_staticPointersByAddress.TryGetValue(pointer.Address, out var startingPoint))
+        {
+            var newPointingThere = new Pointer
+            {
+                ModuleName = startingPoint.ModuleName,
+                BaseAddress = startingPoint.BaseAddress,
+                BaseOffset = startingPoint.BaseOffset,
+                Offsets = pointer.Offsets,
+                PointingTo = pointerScanOptions.SearchedAddress
+            };
+
+            lock (pointerWithStaticPointerPaths)
+            {
+                pointerWithStaticPointerPaths.Add(newPointingThere);
+            }
+        }
+
+        if (currentLevel == pointerScanOptions.MaxLevel - 1)
+        {
+            return;
+        }
+
+        var foundPointersWithOffset = FindHeapPointersWithOffsetWith(pointer, pointerScanOptions.MaxOffset);
+
+        lock (_pointerList)
+        {
+            _pointerList.AddRange(foundPointersWithOffset);
+        }
+    }
+
+    private List<Pointer> FindHeapPointersWithOffsetWith(Pointer pointer, int maxOffset)
+    {
+        var foundPointers = new List<Pointer>();
+
+        for (int currentOffset = 0; currentOffset < maxOffset; currentOffset += _pointerSize)
+        {
+            var newAddress = pointer.Address - currentOffset;
+            if (_heapPointersByPointingTo.TryGetValue(newAddress, out var pointers))
+            {
+                if (pointers == null)
+                    continue;
+
+                var offsets = new List<IntPtr>(pointer.Offsets)
+                {
+                    currentOffset
+                };
+
+                foreach (var foundPointer in pointers)
+                {
+                    var clonedPointer = foundPointer.Clone();
+                    clonedPointer.Offsets = offsets.ToList();
+                    foundPointers.Add(clonedPointer);
+                }
+
+                var countingFound = _pathCounter.TryGetValue(newAddress, out int count);
+                if (!_staticPointersByAddress.ContainsKey(newAddress) && countingFound && count >= 3)
+                {
+                    lock (_heapPointersByPointingTo)
                     {
-                        var newPointingThere = new Pointer
-                        {
-                            ModuleName = startingPoint.ModuleName,
-                            BaseAddress = startingPoint.BaseAddress,
-                            BaseOffset = startingPoint.BaseOffset,
-                            Offsets = pointer.Offsets,
-                            PointingTo = pointerScanOptions.SearchedAddress
-                        };
-                        pointingThere.Add(newPointingThere);
+                        _heapPointersByPointingTo.Remove(newAddress);
                     }
-
-                    if (alreadyTracking.Contains(pointer.Address))
+                }
+                else
+                {
+                    lock (_pathCounter)
                     {
-                        continue;
-                    }
-
-                    if (currentLevel == pointerScanOptions.MaxLevel - 1)
-                    {
-                        continue;
-                    }
-
-                    var newAddy = IntPtr.Zero;
-                    for (int i = 0; i < pointerScanOptions.MaxOffset; i += _pointerSize)
-                    {
-
-                        newAddy = pointer.Address - i;
-                        if (heapPointersByPointingTo.TryGetValue(newAddy, out var pointers))
-                        {
-                            var clonedPointers = pointers.Select(x => x.Clone()).ToList();
-                            var offsets = new List<IntPtr>(pointer.Offsets)
-                        {
-                            i
-                        };
-                            clonedPointers.ForEach(x => x.Offsets = new List<IntPtr>(offsets));
-                            pointerScan1.AddRange(clonedPointers);
-                            var countingFound = counter.TryGetValue(newAddy, out int count);
-                            if (!staticPointersByAddress.ContainsKey(newAddy) && countingFound && count >= 3)
-                            {
-                                heapPointersByPointingTo.Remove(newAddy);
-                            }
-                            else
-                            {
-                                count++;
-                                if (!countingFound)
-                                {
-                                    counter.Add(newAddy, count);
-                                }
-                                counter[newAddy] = count;
-                            }
-                        }
+                        _pathCounter[newAddress] = ++count;
                     }
                 }
             }
+        }
 
-            return pointingThere.OrderBy(x => x.Offsets.Count).ToList();
-        }).ConfigureAwait(false);
-
-        return result;
+        return foundPointers;
     }
 
     public async Task<IList<Pointer>> RescanPointersAsync(IEnumerable<Pointer> pointers, int processId, IntPtr processHandle, IntPtr searchedAddress)
