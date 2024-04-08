5c1f0d714412c16230aa7ebe4ad31a09475cfada
SavasCelik <scelik.ch@gmail.com>
2024-04-08 11:29:57 +0200

    Added tailwind

---
+++ src/CelSerEngine.WpfBlazor/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: ["./**/*.{razor,html,cshtml,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
