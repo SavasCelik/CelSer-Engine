c547054051491ba7e44ef59632e0719fa320987d
SavasCelik <scelik.ch@gmail.com>
2024-04-09 03:10:35 +0200

    Remove tailwindcss

--- src/CelSerEngine.WpfBlazor/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    darkMode: 'class',
-    content: ["./**/*.{razor,html,cshtml,js}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
