93c6544bc67a507895d8699afcdc579b063d50ba
SavasCelik <scelik.ch@gmail.com>
2024-04-22 17:21:05 +0200

    Change selected process

--- src/CelSerEngine.WpfBlazor/Components/SelectProcess.razor.cs
+++ src/CelSerEngine.WpfBlazor/Components/SelectProcess.razor.cs
@@ -1,16 +1,25 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using CelSerEngine.Core.Native;
+using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using System.Diagnostics;
 using System.Text.Json;
 
 namespace CelSerEngine.WpfBlazor.Components;
 
-public partial class SelectProcess : ComponentBase
+public partial class SelectProcess : ComponentBase, IDisposable
 {
     private List<ProcessAdapter> _processes = new();
 
     [Inject]
-    public IJSRuntime JS { get; set; } = default!;
+    private IJSRuntime JS { get; set; } = default!;
+
+    [Inject]
+    private EngineSession EngineSession { get; set; } = default!;
+
+    [Inject]
+    private INativeApi NativeApi { get; set; } = default!;
+
+    private DotNetObjectReference<SelectProcess>? _dotNetHelper;
 
     protected override async Task OnInitializedAsync()
     {
@@ -27,8 +36,26 @@ public partial class SelectProcess : ComponentBase
     {
         if (firstRender)
         {
-            var module = await JS.InvokeAsync<IJSObjectReference>("import", "/js/selectProcess.js");
-            await module.InvokeVoidAsync("ready", JsonSerializer.Serialize(_processes.Select((x, i) => new { Name = x.DisplayString, x.IconBase64Source }).ToList()));
+            _dotNetHelper = DotNetObjectReference.Create(this);
+            var module = await JS.InvokeAsync<IJSObjectReference>("import", "./Components/SelectProcess.razor.js");
+            await module.InvokeVoidAsync("ready", JsonSerializer.Serialize(_processes.Select((x, i) => new { Name = x.DisplayString, x.IconBase64Source, x.Process.Id })), _dotNetHelper);
+            await module.DisposeAsync();
         }
+    }
+
+    [JSInvokable]
+    public void SetSelectedProcessById(int processId)
+    {
+        var process = Process.GetProcessById(processId);
+        EngineSession.SelectedProcess = new ProcessAdapter(process)
+        {
+            ProcessHandle = NativeApi.OpenProcess(processId)
+        };
+    }
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        _dotNetHelper?.Dispose();
     }
 }
--- src/CelSerEngine.WpfBlazor/Components/SelectProcess.razor.js
+++ src/CelSerEngine.WpfBlazor/Components/SelectProcess.razor.js
@@ -1,7 +1,9 @@
 let gridApi;
 let filterTextField;
+let dotNetHelper;
 
-function ready(data) {
+function ready(data, _dotNetHelper) {
+    dotNetHelper = _dotNetHelper;
     filterTextField = document.querySelector("#filter-text-field");
     filterTextField.addEventListener("input", onFilterTextFieldChanged)
     filterTextField.focus();
@@ -26,16 +28,15 @@ function ready(data) {
                 cellRenderer: (params) => `<img height="20" width="20" src="data:image/png;base64,${params.data.IconBase64Source}" /> <span>${params.data.Name}</span>`
             }
         ],
-        onRowDoubleClicked: doSomething
+        onRowDoubleClicked: selectProcess
     };
 
     const myGridElement = document.querySelector('#myGrid');
     gridApi = agGrid.createGrid(myGridElement, gridOptions);
 }
 
-function doSomething(row) {
-    console.log(row);
-    console.log(row.data);
+async function selectProcess(row) {
+    await dotNetHelper.invokeMethodAsync("SetSelectedProcessById", row.data.Id)
 }
 
 function onFilterTextFieldChanged() {
