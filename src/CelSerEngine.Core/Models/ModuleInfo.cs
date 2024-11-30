925bb1befa78af134b78e6a062a46f04201b58bb
SavasCelik <scelik.ch@gmail.com>
2024-11-30 19:28:53 +0100

    Introduce new field for short name of module

--- src/CelSerEngine.Core/Models/ModuleInfo.cs
+++ src/CelSerEngine.Core/Models/ModuleInfo.cs
@@ -7,7 +7,28 @@ public class ModuleInfo
     public uint Size { get; set; }
     public int ModuleIndex { get; set; }
 
+    private string? _shortName;
+
+    public string ShortName
+    {
+        get
+        {
+            if (_shortName != null) 
+                return _shortName;
+
+            var name = Name;
+            var index = name.LastIndexOf('\\');
+
+            if (index != -1)
+                name = name.Substring(index + 1);
+
+            _shortName = name;
+            return _shortName;
+        }
+    }
+
     private bool? _isSystemModule;
+
     public bool IsSystemModule 
     {
         get
