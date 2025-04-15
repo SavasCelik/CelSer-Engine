90ba66a9ecc44a1badc668e7c89cd3b71de2156e
Savas Celik <scelik.ch@gmail.com>
2025-04-15 22:31:40 +0200

    Fix format and name changes

--- src/celserengine.reactclient/src/utils/JsInterop.ts
+++ src/celserengine.reactclient/src/utils/JsInterop.ts
@@ -1,81 +1,112 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 interface PendingAsyncCall<T> {
-    resolve: (value?: T | PromiseLike<T>) => void;
-    reject: (reason?: any) => void;
+  resolve: (value?: T | PromiseLike<T>) => void;
+  reject: (reason?: any) => void;
 }
 
 class JsInterop {
-    private componentRegistry: Map<string, any> = new Map<string, any>();
-    private readonly _pendingAsyncCalls: { [id: number]: PendingAsyncCall<any> } = {};
-    private _requestId = 0;
+  private componentRegistry: Map<string, any> = new Map<string, any>();
+  private readonly _pendingAsyncCalls: { [id: number]: PendingAsyncCall<any> } =
+    {};
+  private _requestId = 0;
 
-    attachDotNetObjectAsync(dotNetTypeName: string, componentId: string): Promise<number> {
-        return this.invokeDotNetMethodAsync<number>(null, "AttachDotNetObject", JSON.stringify([ dotNetTypeName, componentId ]));
+  attachDotNetObjectAsync(
+    dotNetTypeName: string,
+    componentId: string
+  ): Promise<number> {
+    return this.invokeDotNetMethodAsync<number>(
+      null,
+      "AttachDotNetObject",
+      JSON.stringify([dotNetTypeName, componentId])
+    );
+  }
+
+  detachDotNetObjectAsync(dotNetRefId: number): Promise<unknown> {
+    return this.invokeDotNetMethodAsync(
+      null,
+      "DetachDotNetObject",
+      JSON.stringify(dotNetRefId)
+    );
+  }
+
+  invokeDotNetMethodAsync<T>(
+    dotNetObjectId?: number | null,
+    methodName?: string,
+    methodArguments?: any
+  ): Promise<T> {
+    const asyncCallId = this._requestId++;
+    const resultPromise = new Promise<T>((resolve, reject) => {
+      this._pendingAsyncCalls[asyncCallId] = { resolve, reject };
+    });
+    const message = JSON.stringify({
+      asyncCallId,
+      dotNetObjectId,
+      methodName,
+      methodArguments,
+    });
+    (window as any).chrome.webview.postMessage(message);
+
+    return resultPromise;
+  }
+
+  handleResponse(event: MessageEvent) {
+    const response = event.data;
+
+    if (
+      response.asyncCallId !== undefined &&
+      Object.prototype.hasOwnProperty.call(
+        this._pendingAsyncCalls,
+        response.asyncCallId
+      )
+    ) {
+      const asyncCall = this._pendingAsyncCalls[response.asyncCallId];
+      delete this._pendingAsyncCalls[response.asyncCallId];
+      asyncCall.resolve(JSON.parse(response.reposeJson));
+    }
+  }
+
+  registerComponent(componentId: string, obj: any): void {
+    const existingComponent = this.componentRegistry.get(componentId);
+
+    if (existingComponent) {
+      throw new Error(
+        `Component with ID ${componentId} is already registered.`
+      );
     }
 
-    detachDotNetObjectAsync(dotNetRefId: number): Promise<unknown> {
-        return this.invokeDotNetMethodAsync(null, "DetachDotNetObject", JSON.stringify(dotNetRefId));
+    this.componentRegistry.set(componentId, obj);
+  }
+
+  invokeComponentMethod(
+    componentId: string,
+    methodName: string,
+    argsAsJson: any
+  ): string {
+    const obj = this.componentRegistry.get(componentId);
+    if (!obj) {
+      throw new Error(`Component with ID ${componentId} is not registered.`);
     }
-
-    invokeDotNetMethodAsync<T>(dotNetObjectId?: number | null, methodName?: string, methodArguments?: any): Promise<T> {
-        const asyncCallId = this._requestId++;
-        const resultPromise = new Promise<T>((resolve, reject) => {
-            this._pendingAsyncCalls[asyncCallId] = { resolve, reject };
-        });
-        const message = JSON.stringify({
-            asyncCallId,
-            dotNetObjectId,
-            methodName,
-            methodArguments,
-        });
-        (window as any).chrome.webview.postMessage(message);
-
-        return resultPromise;
+    const method = obj[methodName];
+    if (typeof method === "function") {
+      //const args = JSON.parse(argsAsJson);
+      return JSON.stringify(method(...argsAsJson));
+    } else {
+      throw new Error(
+        `Method ${methodName} is not defined on component with ID ${componentId}.`
+      );
     }
+  }
 
-    handleResponse(event: MessageEvent) {
-        const response = event.data
-
-        if (response.asyncCallId !== undefined && Object.prototype.hasOwnProperty.call(this._pendingAsyncCalls, response.asyncCallId)) {
-            const asyncCall = this._pendingAsyncCalls[response.asyncCallId];
-            delete this._pendingAsyncCalls[response.asyncCallId];
-            asyncCall.resolve(JSON.parse(response.reposeJson));
-        }
-    }
-
-    registerComponent(componentId: string, obj: any): void {
-        const existingComponent = this.componentRegistry.get(componentId);
-
-        if (existingComponent) {
-            throw new Error(`Component with ID ${componentId} is already registered.`);
-        }
-
-        this.componentRegistry.set(componentId, obj);
-    }
-
-    invokeComponentMethod(componentId: string, methodName: string, argsAsJson: any): string {
-        const obj = this.componentRegistry.get(componentId);
-        if (!obj) {
-            throw new Error(`Component with ID ${componentId} is not registered.`);
-        }
-        const method = obj[methodName];
-        if (typeof method === 'function') {
-            //const args = JSON.parse(argsAsJson);
-            return JSON.stringify(method(...argsAsJson));
-        } else {
-            throw new Error(`Method ${methodName} is not defined on component with ID ${componentId}.`);
-        }
-    }
-
-    unregisterComponent(componentId: string): boolean {
-        return this.componentRegistry.delete(componentId);
-    }
+  unregisterComponent(componentId: string): boolean {
+    return this.componentRegistry.delete(componentId);
+  }
 }
 
 // Expose the JsInterop instance globally
 
 export const jsInteropObj = new JsInterop();
-(window as any).chrome.webview.addEventListener("message", (event: any) => jsInteropObj.handleResponse(event));
+(window as any).chrome.webview.addEventListener("message", (event: any) =>
+  jsInteropObj.handleResponse(event)
+);
 
 (window as any)["jsInterop"] = jsInteropObj;
-
--- src/celserengine.reactclient/src/utils/useDotNet.tsx
+++ src/celserengine.reactclient/src/utils/useDotNet.tsx
@@ -1,43 +1,53 @@
 import { useEffect, useRef, useState } from "react";
-import { jsInteropObj } from './JsInterop2_1';
+import { jsInteropObj } from "./JsInterop";
 
 class DotNetObject {
-    constructor(private objectId: number) { }
-    invokeMethod<T>(methodName: string, ...args: any[]) {
-        return jsInteropObj.invokeDotNetMethodAsync<T>(this.objectId, methodName, JSON.stringify(args));
-    }
+  constructor(private objectId: number) {}
+  invokeMethod<T>(methodName: string, ...args: any[]) {
+    return jsInteropObj.invokeDotNetMethodAsync<T>(
+      this.objectId,
+      methodName,
+      JSON.stringify(args)
+    );
+  }
 
-    async detach() {
-        await jsInteropObj.DetachDotNetObjectAsync(this.objectId);
-    }
+  async detach() {
+    await jsInteropObj.detachDotNetObjectAsync(this.objectId);
+  }
 }
 
-export function useDotNet(componentId: string, dotNetClassName: string): DotNetObject | null {
-    const dotNetObjectRef = useRef<DotNetObject | null>(null);
-    const [dotNetObject, setDotNetObject] = useState<DotNetObject | null>(null);
+export function useDotNet(
+  componentId: string,
+  dotNetClassName: string
+): DotNetObject | null {
+  const dotNetObjectRef = useRef<DotNetObject | null>(null);
+  const [dotNetObject, setDotNetObject] = useState<DotNetObject | null>(null);
 
-    useEffect(() => {
-        const registerDotNetObject = async () => {
-            const dotNetObjectId = await jsInteropObj.AttachDotNetObjectAsync(dotNetClassName, componentId);
+  useEffect(() => {
+    const registerDotNetObject = async () => {
+      const dotNetObjectId = await jsInteropObj.attachDotNetObjectAsync(
+        dotNetClassName,
+        componentId
+      );
 
-            if (dotNetObjectRef.current != null) {
-                await dotNetObjectRef.current.detach();
-            }
+      if (dotNetObjectRef.current != null) {
+        await dotNetObjectRef.current.detach();
+      }
 
-            dotNetObjectRef.current = new DotNetObject(dotNetObjectId);
-            setDotNetObject(dotNetObjectRef.current);
-        };
+      dotNetObjectRef.current = new DotNetObject(dotNetObjectId);
+      setDotNetObject(dotNetObjectRef.current);
+    };
 
-        registerDotNetObject();
+    registerDotNetObject();
 
-        return () => {
-            const detachDotNetObject = async () => {
-                await dotNetObjectRef.current?.detach();
-            }
+    return () => {
+      const detachDotNetObject = async () => {
+        await dotNetObjectRef.current?.detach();
+      };
 
-            detachDotNetObject();
-        };
-    }, [componentId, dotNetClassName]);
+      detachDotNetObject();
+    };
+  }, [componentId, dotNetClassName]);
 
-    return dotNetObject;
+  return dotNetObject;
 }
