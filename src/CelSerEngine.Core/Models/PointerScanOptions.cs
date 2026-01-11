140cfb968ba93d28f7f6fe36bf9c73b8b8e29db6
SavasCelik <scelik.ch@gmail.com>
2026-01-11 19:41:40 +0100

    Introduced the RequireAlignedPointers property in PointerScanOptions to control whether only 4-byte aligned pointers are scanned.

--- src/CelSerEngine.Core/Models/PointerScanOptions.cs
+++ src/CelSerEngine.Core/Models/PointerScanOptions.cs
@@ -7,6 +7,7 @@ public class PointerScanOptions
     public int ProcessId { get; set; }
     public SafeProcessHandle ProcessHandle { get; set; }
     public IntPtr SearchedAddress { get; set; }
+    public bool RequireAlignedPointers { get; set; }
     public int MaxLevel { get; set; }
     public int MaxOffset { get; set; }
 
